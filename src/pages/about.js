0524adb7b495b059f31156ea351ba66f6c3c1aab
Iza <iza.moerenhout@hotmail.com>
2023-01-17 20:45:05 +0100

    Changed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,14 +17,6 @@ function About() {
             well you can adapt to taking classes online or working from home.
           </p>
 
-          <h2>How does it work?</h2>
-          <p>
-            This AI can predict how well you are likely to adapt to online
-            classes. The AI makes this prediction based on your entered data and
-            a trained model. With the click of a button, it is predicted how
-            well you can adapt to taking classes online or working from home.
-          </p>
-
           <h2>How do we handle your data?</h2>
           <p>
             The entered data is used only once by our trained AI. That is why it
